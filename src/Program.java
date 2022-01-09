9c2c1e4206b0ef72d41e822ebf42227d2e38703f
ImFreeman <denikvy@gmail.com>
2022-01-09 12:30:53 +0300

    Сделано!

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,15 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Program {
     public static void main(String[] str){
-        
+        List<Integer> arr = IntStream.range(1, 101).boxed().collect(Collectors.toList());
+        arr.set(4, 89);
+
+        int duplicateSum = arr.stream().distinct().mapToInt(e->e).sum();
+        int totalSum = arr.stream().mapToInt(e->e).sum();
+        int result = totalSum - duplicateSum;
+        System.out.println("Число которое повторяется " + result);
     }
 }
